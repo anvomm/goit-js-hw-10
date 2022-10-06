7e266e97a745cd90997bbb593150df5e7516abcd
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-10-06 17:03:02 +0300

    height attribute removed

--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -7,7 +7,7 @@ export function renderOneCard(array) {
     languages,
   } = array[0];
   const languagesList = languages.map(language => language.name).join(', ');
-  return `<div class="country__wrap"><img alt="${name} flag" width=30 height=30 src="${svg}"/><h2 class="country__name">${name}</h2></div><ul class="country__list"><li><span class="country__property">Capital:</span> ${capital}</li><li><span class="country__property">Population:</span> ${population}</li><li><span class="country__property">Languages:</span> ${languagesList}</li></ul>`;
+  return `<div class="country__wrap"><img alt="${name} flag" width=30 src="${svg}"/><h2 class="country__name">${name}</h2></div><ul class="country__list"><li><span class="country__property">Capital:</span>${capital}</li><li><span class="country__property">Population:</span> ${population}</li><li><span class="country__property">Languages:</span> ${languagesList}</li></ul>`;
 }
 
 export function renderManyCards(array) {
@@ -17,7 +17,7 @@ export function renderManyCards(array) {
         flags: { svg },
         name,
       } = country;
-      return `<li><img alt="${name} flag" width=30 height=30 src="${svg}"/><p>${name}</p></li>`;
+      return `<li><img alt="${name} flag" width=30 src="${svg}"/><p>${name}</p></li>`;
     })
     .join('');
 }
